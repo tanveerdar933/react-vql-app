978cedf7413cf6a0f2a45fdb89b98b84faf052d6
tanveerdar933 <tanveerdar933@gmail.com>
2023-11-29 00:35:38 +0500

    added qrCode generation

--- src/pages/ScanQr.jsx
+++ src/pages/ScanQr.jsx
@@ -1,6 +1,40 @@
+import { useEffect, useRef } from "react";
+
 const ScanQr = () => {
+  const qrRef = useRef(null);
+  function onScan(data) {
+    //do something with the result
+    console.log('onScan', data);
+  }
+  function onInit(data) {
+    console.log('onInit', data);
+  }
+  useEffect(() => {
+    if (qrRef && qrRef.current) {
+      qrRef.current.setAttribute("queryId", "6565c38081738c00554d0334");
+      qrRef.current.setAttribute("qrcodeOwner", "shareledger129j2ru5mvj9v2c7af9ym8keu844ca27myasuue");
+      qrRef.current.addEventListener("oninit", onInit);
+      qrRef.current.addEventListener("onscan", onScan);
+    }
+    return () => {
+      qrRef.current.removeEventListener("oninit", onInit);
+      qrRef.current.removeEventListener("onscan", onScan);
+    }
+  }, [qrRef]);
+
+
   return (
-    <div>ScanQr</div>
+    <div
+      ref={qrRef}
+      id="qrContainer"
+      mode="dynamic"
+      app="Identifi Me"
+      className="sharering-query"
+    >
+      <div className="qrcode-content">
+        <div id="qrcode"></div>
+      </div>
+    </div>
   )
 }
 
